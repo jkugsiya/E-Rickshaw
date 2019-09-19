8cc58d23ac5b461e5b613bda9cb3cd01088a6743
Jayesh Kugsiya <jkugsiya11@gmail.com>
2019-09-19 09:11:59 +0530

    First  commit by Jayesh Kugsiya
    The Initial Project done till two canvas.
    The work starting from the second canvas is remaining.

---
+++ ErickshawAr Without lwrp/Assets/Scripts/canvas2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvas2 : MonoBehaviour
+{
+
+    public GameObject rickshaw;
+    public GameObject button;
+    public GameObject UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void verify()
+    {
+        rickshaw.SetActive(false);
+        button.SetActive(false);
+        UI.SetActive(true);
+    }
+}
